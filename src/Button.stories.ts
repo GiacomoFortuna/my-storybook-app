f3f592e3f3656b5d5c12b3788f3b5a1f2addf51e
GiacomoFortuna <fortunagiacomo03@gmail.com>
2025-04-14 13:12:36 +0000

    Implementata storia con interazione

--- src/Button.stories.ts
+++ src/Button.stories.ts
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from './Button';
+import { within } from '@testing-library/react';
+import { userEvent} from '@storybook/testing-library';
 
 const meta: Meta<typeof Button> = {
     component: Button,
@@ -30,4 +32,15 @@ export const Disabled: Story = {
         },
         disabled: false,
     },
+};
+export const WithInteraction: Story = {
+    args: {
+        label: 'Button with Interaction',
+        onClick: () => alert('Clicked!'),
+    },
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement);
+        const button = canvas.getByRole('button');
+        await userEvent.click(button);
+    }
 };
